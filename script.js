0c1f2feec118a4ff45f3636b57d4d740e4b507af
Sinan386 <sinan.demiroz@le-campus-numerique.fr>
2025-04-30 15:30:08 +0200

    Page d'accueil feed et début du formulaire

--- script.js
+++ script.js
@@ -1,19 +1,66 @@
-// async function afficherJoke() {
-//     const reponse = await fetch("https://v2.jokeapi.dev/joke/Any?lang=fr");
-//     const joke = await reponse.json();
-//     console.log(joke);
-//   }
+const container = document.getElementById("jokes");
 
-//   afficherJoke();
+const buttonReload = document.getElementById("reloadJokes");
+buttonReload.addEventListener("click", function () {
+  // clear jokes container
+  container.innerHTML = "";
 
-foreach ( i = 0; i < 10; i++) {
-fetch("https://v2.jokeapi.dev/joke/Any?lang=fr")
-    .then(res => res.json())
-    .then(joke => {
-        console.log(joke)
-       let a = joke;
-       });
+  // add new jokes
+  getJokes(4); // Je veux afficher 5 blagues
+});
+
+/**
+ * Fonction qui affiche une blague à l'écran
+ * @param question la 1ère partie de la blague (question)
+ * @param answer la 2nd partie de la blague (réponse)
+ * @param index le numéro de la blague
+ */
+function displayJoke(question, answer, index) {
+  const child = document.createElement("div");
+  child.className = "joke-card";
+  container.appendChild(child);
+
+  child.innerHTML = `${index}: <p>${question}</p><p>${answer}</p>`;
 }
-    
 
-    
+/**
+ * Fonction qui récupère une blague depuis une API distante et l'affiche dans l'index.html
+ * @param index le numéro de la blague
+ */
+function getJoke(index) {
+  fetch("https://v2.jokeapi.dev/joke/Any?lang=fr")
+    .then((res) => res.json())
+    .then(function (json) {
+      displayJoke(json.setup, json.delivery, index);
+    });
+}
+
+/**
+ * Fonction qui affiche une seule blague avec son numéro en paramètre
+ * @param jokeNum le numéro de la blague
+ */
+function getJokes(jokeNum) {
+  for (let i = 0; i < jokeNum; i++) {
+    getJoke(i + 1);
+  }
+}
+
+getJokes(5); // Je veux afficher 5 blagues
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
